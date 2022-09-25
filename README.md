8a6c947b8a507f388ff69265f057061602b1aba8
nick thomas <nichoth@gmail.com>
2022-09-25 11:23:44 -0700

    readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# bundle size
+
+Looking at the bundle sizes of various view libraries.
+
+```
+bundle-size main % ll preact 
+-rw-r--r--  1 nick  staff    13K Sep 25 11:17 bundle.js
+
+bundle-size main % ll tonic 
+-rw-r--r--  1 nick  staff   9.2K Sep 25 11:17 bundle.js
+```
--- preact/index.js
+++ preact/index.js
@@ -1,9 +1,15 @@
 import { render } from 'preact';
 import { html } from 'htm/preact';
-import { useState } from 'preact/hooks';
+// we're not really using `useEffect` here. It is included just to get
+//   a comparable bundle size
+import { useState, useEffect } from 'preact/hooks';
 
 function Hello () {
     const [target, setTarget] = useState('world')
+    useEffect(() => {
+        setTarget('earth')
+    }, [])
+
     return html`<p>Hello, ${target}</p>`
 }
 
